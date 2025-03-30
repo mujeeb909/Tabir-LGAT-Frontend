1a9781e35956fc8934e1897a439a25a929312b4a
Muhammad Mujeeb <mohammad.mujeeb.051@gmail.com>
2025-03-30 06:21:53 +0500

    updates

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    // ⚠️ Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // ⚠️ Also ignore ESLint errors during builds
+    ignoreDuringBuilds: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
